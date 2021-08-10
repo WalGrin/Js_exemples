1f5befb422579b7b1d10eeecb1dd0120d659d578
ValeryGrinko <grinko.vr@intechs.by>
2021-08-10 17:14:23 +0300

    prototype example

---
+++ js/proto.js
@@ -0,0 +1,6 @@
+function Rabbit() {}
+// по умолчанию:
+// Rabbit.prototype = { constructor: Rabbit }
+
+let rabbit = new Rabbit(); // наследует от {constructor: Rabbit}
+console.log(rabbit.constructor == Rabbit);
